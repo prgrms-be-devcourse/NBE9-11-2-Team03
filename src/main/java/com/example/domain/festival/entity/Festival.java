341cfda65dfb43ea970110132d405aa0a0b2e677
HanJeongmok <wjdahe1845@gmail.com>
2026-04-14 11:14:15 +0900

    [fix]:축제엔티티 likeCount=>북마크 카운트로 수정

--- src/main/java/com/example/domain/festival/entity/Festival.java
+++ src/main/java/com/example/domain/festival/entity/Festival.java
@@ -87,7 +87,7 @@ public class Festival extends BaseEntity {
         // 기본값 세팅
         this.status = FestivalStatus.UPCOMING;
         this.viewCount = 0;
-        this.likeCount = 0;
+        this.bookMarkCount = 0;
         this.averageRate = 0.0;
     }
 }
