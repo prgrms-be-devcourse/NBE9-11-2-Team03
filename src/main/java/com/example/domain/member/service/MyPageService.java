3797cdf2b1ca0f698b656db53dfe31c0a7e9a72e
HanJeongmok <wjdahe1845@gmail.com>
2026-04-16 16:54:10 +0900

    [feat]:탈퇴한 사용자 조회못하게 에러처리

--- src/main/java/com/example/domain/member/service/MyPageService.java
+++ src/main/java/com/example/domain/member/service/MyPageService.java
@@ -5,10 +5,12 @@ import com.example.domain.festival.entity.Festival;
 import com.example.domain.festival.repository.FestivalRepository;
 import com.example.domain.member.dto.response.MyPageRes;
 import com.example.domain.member.entity.Member;
+import com.example.domain.member.entity.MemberStatus;
 import com.example.domain.member.repository.MemberRepository;
 import com.example.domain.review.repository.ReviewRepository;
 import com.example.domain.review.service.ReviewService;
 import com.example.global.exception.CustomNotFoundException;
+import com.example.global.exception.ForbiddenException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +24,9 @@ public class MyPageService {
 
     public MyPageRes getMyPage(String loginId) {
         Member member = memberRepository.findByLoginId(loginId).orElseThrow(()->new CustomNotFoundException("로그인한 회원 정보를 찾을 수 없습니다."));
+        if(member.getStatus()== MemberStatus.WITHDRAWN){
+            throw new ForbiddenException("탈퇴한 회원은 마이페이지를 조회할 수 없습니다.");
+        }
         long reviewCount = reviewRepository.countByMemberId(member.getId());
         long bookMarkCount= festivalBookmarkRepository.countByMemberId(member.getId());
         return new MyPageRes(
