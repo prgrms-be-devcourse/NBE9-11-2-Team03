960efc391a075f6852d286b8a7d117dfd19de1a9
HanJeongmok <wjdahe1845@gmail.com>
2026-04-17 15:06:58 +0900

    마이페이지 자신이 찜한 축제 목록을 확인 할 수 있다. (#33)
    * Feature/entity constraints (#2)
    
    * refactor: 엔티티 제약조건 설정
    
    * feat: ReviewReportRepository 추가
    
    * fix:중복된 코드수정
    
    * fix:오타수정
    
    * [fix] 깃 충돌 해결
    
    * [fix]:의존성문제 해결
    
    * 병합 문제 해결
    
    * [fix] conflit해결
    
    * feat:myPage기능작성
    
    * fix(병합 해결
    
    * refactor:주석달기
    
    ---------
    
    Co-authored-by: wlstp8473 <65928720+wlstp8473@users.noreply.github.com>

--- src/main/java/com/example/domain/bookmark/repository/FestivalBookmarkRepository.java
+++ src/main/java/com/example/domain/bookmark/repository/FestivalBookmarkRepository.java
@@ -1,8 +1,13 @@
 package com.example.domain.bookmark.repository;
 
 import com.example.domain.bookmark.entity.FestivalBookmark;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FestivalBookmarkRepository extends JpaRepository<FestivalBookmark,Long> {
     long countByMemberId(long memberId);
+    @Query("SELECT fb FROM FestivalBookmark fb JOIN FETCH fb.festival WHERE fb.member.id = :memberId")
+    Page<FestivalBookmark> findByMemberId(Long memberId, Pageable pageable);
 }
