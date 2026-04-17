9a259ef6d0a265d78e601de1f548b458990b8a3d
chiken177@naver.com <alsgur3161@naver.com>
2026-04-17 17:24:28 +0900

    feat:축제 찜하기 및 찜 취소 API 구현

--- src/main/java/com/example/domain/bookmark/repository/FestivalBookmarkRepository.java
+++ src/main/java/com/example/domain/bookmark/repository/FestivalBookmarkRepository.java
@@ -3,6 +3,13 @@ package com.example.domain.bookmark.repository;
 import com.example.domain.bookmark.entity.FestivalBookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FestivalBookmarkRepository extends JpaRepository<FestivalBookmark,Long> {
+
+    boolean existsByMemberIdAndFestivalId(Long memberId, Long festivalId);
+
     long countByMemberId(long memberId);
+
+    Optional<FestivalBookmark> findByMemberIdAndFestivalId(Long memberId, Long festivalId);
 }
